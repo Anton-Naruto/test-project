930593669e316de41b8ddeccae312b82d7fe1d0f
Anton-Naruto <antochin96@gmail.com>
2022-09-07 21:11:50 +0200

    4: pigfarm

--- src/com/antochin/anton/FirstProgram.java
+++ src/com/antochin/anton/FirstProgram.java
@@ -2,13 +2,20 @@ package com.antochin.anton;
 
 public class FirstProgram {
 
-    public static void main(String [] args){
-        int count = countPidors();
-        System.out.println(count + " pidorow gorit w adu");
-        System.out.println("ja pierdole");
-    }
+    public static void main(String[] args) {
+        Pig hruk = new Pig("hruk", 56);
+        Pig lolka = new Pig("lolka", 70);
+        Pig natasha = new Pig("natasha", 87);
+        Pig gala = new Pig("gala", 21);
+        Pig nastya = new Pig("nastya", 33);
 
-    public static int countPidors(){
-        return 10;
+        System.out.println(hruk.getName() + ": " + hruk.getWeight() +" kg");
+        System.out.println(lolka.getName() + ": " + lolka.getWeight() +" kg");
+        System.out.println(natasha.getName() +" :" + natasha.getWeight() +"kg ");
+        System.out.println(gala.getName() +" :" + gala.getWeight() + "kg");
+        System.out.println(nastya.getName() + " :" + nastya.getWeight() + "kg");
+
+        int weight = hruk.getWeight() + lolka.getWeight() + natasha.getWeight() + gala.getWeight() + nastya.getWeight();
+        System.out.println("counting : " + weight);
     }
 }
---
+++ src/com/antochin/anton/Pig.java
@@ -0,0 +1,19 @@
+package com.antochin.anton;
+
+public class Pig {
+    private String name;
+    private int weight;
+
+    public Pig(String name, int weight) {
+        this.name = name;
+        this.weight = weight;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getWeight() {
+        return weight;
+    }
+}
