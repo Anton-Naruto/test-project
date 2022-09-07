87f41856536e7b421d4953f74aa1b37fa4671bfc
Anton-Naruto <antochin96@gmail.com>
2022-09-07 19:53:40 +0200

    3: add ja pierdole

--- src/com/antochin/anton/FirstProgram.java
+++ src/com/antochin/anton/FirstProgram.java
@@ -5,6 +5,7 @@ public class FirstProgram {
     public static void main(String [] args){
         int count = countPidors();
         System.out.println(count + " pidorow gorit w adu");
+        System.out.println("ja pierdole");
     }
 
     public static int countPidors(){
